87ac4acd9a9e99bf291c9ecf06b44d1307cfb7ce
juleshenry <julianplushenry@gmail.com>
2023-05-07 18:26:46 -0500

    /Users/mariopokemon/Desktop/projs/git-goblin/gacp

--- kwarg_solver_decorator.py
+++ kwarg_solver_decorator.py
@@ -1,21 +1,41 @@
+import inspect
+
 def kwarg_solver(func):
-    def solver_decorator():
+    def wrapper(*args, **kwargs):
         print("Something is happening before the function is called.")
-        func()
+        func(*args, **kwargs)
         print("Something is happening after the function is called.")
     return wrapper
 
 class Einstein:
 
-    @kwarg_solver
-    def einstein(e=None, m=None):
-        
-        return 
+    def einstein_1(s,**kwargs):
+        methname = inspect.stack()[0][3]
+        missing_arg = list(filter(lambda x:x,kwargs))
+        if not len(missing_arg) == 1:
+            raise IllegalArgumentException
+        correct_method = methname + '__'+missing_arg[0] 
+        for self_attr in dir(s):
+            if self_attr == correct_method:
+                args = [x[1] for x in sorted(kwargs.items(),key=lambda kv:kv[0])]
+                return getattr(s,correct_method)(*args)
+        return 0
 
-    def einstein_
+    def einstein_1__m(s, e:float):
+        return e / 8.98755179e16
 
-    def einstein_e(m: float) -> float:
-        return m * 8.98755179 e16
 
-einstein(e = 1000) # returns m, (1000 / 8.98755179 e16), ~1.11265 e -14
-einstein(m = 1000) # returns e, 1000 * 8.98755179 e16, ~8.98755179 e19
+    def einstein_1__e(s, m: float) -> float:
+        return m * 8.98755179e16
+
+e = Einstein()
+ans = e.einstein_1(e = 1000) # returns m, (1000 / 8.98755179 e16), ~1.11265 e -14
+print(ans)
+ans = e.einstein_1(m = 1000) # returns e, 1000 * 8.98755179 e16, ~8.98755179 e19
+print(ans)
+
+# class A:
+#     def o(s):
+#         for k in dir(s):
+#             print(k)
+# A().o()
