9fc680b5e8919159548aa47435ed6779df5cb6b4
juleshenry <julianplushenry@gmail.com>
2023-05-08 10:46:50 -0500

    /Users/mariopokemon/Desktop/projs/git-goblin/gacp

--- kwarg_solver_decorator.py
+++ kwarg_solver_decorator.py
@@ -1,15 +1,23 @@
+
+
 def kwarg_solver(func):
+    """
+    Intention: 
+    base method is `vanilla`
+    To calculate necessary 
+    
+    formula to solve for a single variable 
+    """
     def wrapper(self, *args, **kwargs):
-        print(kwargs)
         method_name = func.__name__
         missing_arg = list(filter(lambda x:x, kwargs))
         if not len(missing_arg) == 1:
             raise IllegalArgumentException
         correct_method = method_name + '__' + missing_arg[0] 
-        print(correct_method)
         correct_args = [x[1] for x in sorted(kwargs.items(), key=lambda kv:kv[0])]
         return getattr(self ,correct_method)(*correct_args)
     return wrapper
+
 
 class Einstein:
 
@@ -29,9 +37,3 @@ ans = e.einstein_1(e = 1000) # returns m, (1000 / 8.98755179 e16), ~1.11265 e -1
 print(ans)
 ans = e.einstein_1(m = 1000) # returns e, 1000 * 8.98755179 e16, ~8.98755179 e19
 print(ans)
-
-# class A:
-#     def o(s):
-#         for k in dir(s):
-#             print(k)
-# A().o()
