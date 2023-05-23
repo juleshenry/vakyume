94c4bdeea730996abfd7b8fee8056d38bde2a989
juleshenry <julianplushenry@gmail.com>
2023-05-23 15:06:45 -0500

    /Users/mariopokemon/Desktop/projs/git-goblin/gacp

--- test_vakyume_lib.py
+++ test_vakyume_lib.py
@@ -3,8 +3,15 @@ if __name__=='__main__':
 	for o in dir(vakyume_lib):
 		stro = str(o)
 		if stro[0].isalpha():
+			print('sublib',stro)
 			sublib = getattr(vakyume_lib,stro)
 			for a in dir(sublib):
 				astro = str(a)
-				sublibmethod = getattr(str(astro))
-		
+				if stro[0].isalpha():
+					x = vars(vakyume_lib)[stro]
+					print(getattr(x,astro))
+
+					# astro = str(a)
+					# sublibmethod = getattr(o, astro)
+					# print(sublibmethod)
+		break
