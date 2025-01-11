6c07af28d72c0717dfc2a0ba86d566dd4e0e6401
juleshenry <julianplushenry@gmail.com>
2025-01-11 11:20:31 -0600

    evalburrito

--- llm.py
+++ llm.py
@@ -1,9 +1,13 @@
 import ollama
 import secrets
-n='\n';null_str=""
 
-def escribir_codigo(eqn, lang,p1=1,p2=0):
-    print(f"solving { eqn }, buddy.")
+n = "\n"
+null_str = ""
+
+
+def escribir_codigo(eqn, lang, p1_i=None, p2_i=None):
+    int(sum([p1_i, p2_i,]))
+    # print(f"solving { eqn }, buddy.")
     fluff = secrets.choice(["C++", "Erlang", "Haskell", "Rust", "Lisp"])
     response = ollama.chat(
         model="llama3:latest",
@@ -21,19 +25,19 @@ def escribir_codigo(eqn, lang,p1=1,p2=0):
                         Hi Doctor Math, I need help solving this {eqn}  Give back the closed form expression for T
                         
                         """,
-                        f"""identify the line that is a one liner for a closed-form expression for T:\n{eqn}"""
-                    )[1]
+                        f"""identify the line that is a one liner for a closed-form expression for T:\n{eqn}""",
+                    )[p1_i]
                 )
                 + (
                     p2 := (
+                        null_str,
                         """print the python code syntax for an equation solving the formula for every value ablated ( taken one at a time out)
                     example: a**2 = b**2 + c**2
 
 
                     winning correct output:  a = [math.sqrt(b**2+c**2),math.sqrt(b**2+c**2),]; b = [math.sqrt(a**2-c**2),math.sqrt(a**2-c**2),];c = [math.sqrt(a**2-b**2),math.sqrt(a**2-b**2),];""",
-                        null_str,
-                        """Give your answer as one line."""
-                    )[1]
+                        """Give your answer as one line.""",
+                    )[p2_i]
                 ),
             },
         ],
@@ -43,26 +47,26 @@ def escribir_codigo(eqn, lang,p1=1,p2=0):
     dummy(p2)
     return n.join(filter(lambda a: a, response["message"]["content"].split(n)))
 
-
+def eval_wrap(ans):
+    max_bounds =[ 0,len(o:=ans.split(n))]
+    for i,j in (s:=enumerate(len(o))):
+        for ii,jj in s:
+            try:
+                eval('\n'.join(o[i:ii]))
+                print('CODE',o[i:ii])
+                if ii - i > max_bounds[1]-max_bounds[0]:
+                    max_bounds = [i,ii]
+            except:
+                pass
+            #O(n^2)
+    print(n.join(o[max_bounds[0]:max_bounds[1]]))
 print(
-    escribir_codigo(
-        eqn="q = W * (359 / M) * (760 / P) * (T / 492) * (1/60)", lang="Python"
+    ans1 := escribir_codigo(
+        eqn="q = W * (359 / M) * (760 / P) * (T / 492) * (1/60)",
+        lang="Python",
+        p1_i=1,
+        p2_i=1,
     )
 )
-# def identifica_importante(headlines):
-#     response = ollama.chat(
-#         model="llama3:latest",
-#         messages=[
-#             {
-#                 "role": "user",
-#                 "content": f"Order these headlines by most important {headlines}. Strictly use one list, ordinal 1,2,3",
-#             }
-#         ],
-#     )
-#     # Parse response
-#     i, importantes = 1, []
-#     for line in response["message"]["content"].split('\n'):
-#         if line.startswith(f"{i}. "):
-#             importantes.append(line.replace(f"{i}. ", "").replace('"', '').replace("'", ""))
-#             i += 1
-#     return importantes
+print(ans2 := escribir_codigo(eqn=ans1, lang=None, p1_i=2, p2_i=2))  # or 2
+eval_wrap(ans)
